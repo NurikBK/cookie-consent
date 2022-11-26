369ec6d78e702ef4a57cba9cd1c47fb392950f1b
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-26 16:58:32 +0600

    finished the modal joke

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const modal = document.querySelector('#modal');
 const closeModalBtn = document.querySelector('#modal-close-btn');
+const modalForm = document.querySelector('#modal-form');
+
+const modalText = document.querySelector('#modal-text');
 
 closeModalBtn.addEventListener('click', () => (modal.style.display = 'none'));
 
 setTimeout(() => {
   modal.style.display = 'inline';
 }, 1500);
+
+modalForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+  </div>
+`;
+
+  setTimeout(() => {
+    document.querySelector('#uploadText').innerText = 'Making the sale...';
+  }, 1500);
+
+  setTimeout(() => {
+    document.querySelector('#modal-inner').innerHTML = `
+    <h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+  }, 3000);
+
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 10000);
+
+});
